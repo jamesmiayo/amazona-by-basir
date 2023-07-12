6a5df63bd170f5fcb33793eafeac573ca1344325
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-07-12 10:33:24 +0800

    66 - 63

--- data.js
+++ data.js
@@ -61,7 +61,7 @@ const data = {
       slug: 'adidas-fit-pant',
       category: 'Pants',
       image: '/images/p4.jpg',
-      price: 65,
+      price: 63,
       countInStock: 5,
       brand: 'Puma',
       rating: 4.5,
