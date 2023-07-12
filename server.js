eacc0f9c0e77b9ba2fc683dba591e29db6d354bf
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-07-12 08:59:30 +0800

    deploy 859am

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get(`https://amazona-by-basir.onrender.com/api/keys/paypal`, (req, res) => {
+app.get(`/api/keys/paypal`, (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
-app.use('https://amazona-by-basir.onrender.com/api/seed', seedRouter);
-app.use('https://amazona-by-basir.onrender.com/api/products', productRouter);
-app.use('https://amazona-by-basir.onrender.com/api/users', userRouter);
-app.use('https://amazona-by-basir.onrender.com/api/orders', orderRouter);
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
